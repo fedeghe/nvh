fe3302e91d2e8a721f16dbdf144e724fef9d0036
fedeghe <fedeghe@gmail.com>
2020-11-07 23:42:52 +0100

    whoooa

--- bin.js
+++
@@ -1,9 +0,0 @@
-const { spawn } = require('child_process');
-
-const nvh = spawn('./nvh');
-
-nvh.on('error', function(err) {
-    console.log('nvh error:');
-    console.log(err);
-    process.exit(0);
-});
